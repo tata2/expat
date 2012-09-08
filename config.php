cf36154c13b933f100e0330bfbb24f36162d73cd
tata2 <alexandre.tarantini@gmail.com>
2012-09-08 07:21:25 -0700

    Ajout :  - admin + admin.php  - Modification config.php et index.php

--- config.php
+++ config.php
@@ -1,7 +1,4 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$db = new PDO('mysql:host=localhost;dbname=expat', 'root', '');
 ?>
+qsd
